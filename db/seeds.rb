4703c1aee7f891821cf93cde75566916f336d217
Dee Race <deerace12@gmail.com>
2018-11-26 13:07:51 -0500

    migrate db, add content to seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Note.create(title: "Shipwreck at Moonset", date: "November 24, 2018", content: "Atomospheric photo taken along the Skeleton Coast of Namibia. Celestial light from above is planet Jupiter. The Skeleton Coast was given its name for the seal and whale bones scattering the shoreline - currently known for shipwrecks.")

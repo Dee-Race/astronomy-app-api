ce830b9a0bb697a90e3ee0c3bb4b42cb5e9806dd
Dee Race <deerace12@gmail.com>
2018-11-26 15:43:32 -0500

    update notes controller

--- app/controllers/api/v1/notes_controller.rb
+++ app/controllers/api/v1/notes_controller.rb
@@ -1,5 +1,7 @@
-class NotesController < ApplicationController
-  before_action :set_note, only: [:show, :update, :destroy]
+module Api::V1
+
+  class NotesController < ApplicationController
+    before_action :set_note, only: [:show, :update, :destroy]
 
   # GET /notes
   def index
@@ -24,28 +26,14 @@ class NotesController < ApplicationController
     end
   end
 
-  # PATCH/PUT /notes/1
-  def update
-    if @note.update(note_params)
-      render json: @note
-    else
-      render json: @note.errors, status: :unprocessable_entity
-    end
-  end
-
-  # DELETE /notes/1
-  def destroy
-    @note.destroy
-  end
-
   private
-    # Use callbacks to share common setup or constraints between actions.
+
     def set_note
       @note = Note.find(params[:id])
     end
 
-    # Only allow a trusted parameter "white list" through.
     def note_params
       params.require(:note).permit(:title, :date, :content)
     end
 end
+end 

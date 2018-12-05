8c323b6ee427293514f68a13aab26460f4255dff
Dee Race <deerace12@gmail.com>
2018-12-05 13:56:59 -0500

    complete notes controller

--- app/controllers/api/v1/notes_controller.rb
+++ app/controllers/api/v1/notes_controller.rb
@@ -31,6 +31,7 @@ module Api::V1
       render json: @note
     else 
       render json: { message: @note.errors }, status: 400
+    end
   end 
 
   def destroy 
@@ -38,17 +39,20 @@ module Api::V1
       render status: 204
     else 
       render json: { message: "Unable to remove this note"}, status: 400
+    end 
   end 
 
-  private
+    private
 
-    # helper method
-    def set_note
-      @note = Note.find_by(id: params[:id])
-    end
+      # helper method
+      def set_note
+       @note = Note.find_by(id: params[:id])
+      end
 
-    def note_params
-      params.require(:note).permit(:title, :date, :content, :submitted_by)
-    end
-end
+      def note_params
+       params.require(:note).permit(:title, :date, :content, :submitted_by)
+      end
+
+  end
 end 
+
